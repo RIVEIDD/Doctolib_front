cadceb285aa245bae330d51462ff91c5bf90a104
Alexandre Dujardin <alexandredujardin@MacBook-Air-de-Alexandre.local>
2024-10-11 15:37:17 +0200

    r

--- src/app/se-connecter/se-connecter.component.ts
+++ src/app/se-connecter/se-connecter.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-se-connecter',
-  standalone: true,
-  imports: [],
   templateUrl: './se-connecter.component.html',
-  styleUrl: './se-connecter.component.scss'
+  styleUrls: ['./se-connecter.component.scss']
 })
 export class SeConnecterComponent {
 
+  constructor(private router: Router) {}
+
+  // Méthode pour rediriger vers la page de connexion
+  goToConnexion() {
+    this.router.navigate(['/connexion']); // Redirige vers la route 'connexion'
+  }
+
+  // Méthode pour rediriger vers la page d'inscription (si nécessaire)
+  goToInscription() {
+    this.router.navigate(['/inscription']); // Redirige vers la route 'inscription'
+  }
 }
